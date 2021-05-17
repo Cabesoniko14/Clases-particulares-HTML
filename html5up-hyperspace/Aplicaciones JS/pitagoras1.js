bbe48a63e00c116664420eec68e1d3253c1709f4
Javi Nieto <iamcabesa@gmail.com>
2021-05-17 15:00:11 -0500

    Primera clase: Pitágoras

---
+++ html5up-hyperspace/Aplicaciones JS/pitagoras1.js
@@ -0,0 +1,27 @@
+var ca, cb;
+var outputText;
+
+    function catetos() {
+
+        ca = document.forms["input_form"]["valora"].value;
+        cb = document.forms["input_form"]["valorb"].value;
+
+        if (ca < 0) {
+            outputText = "El valor de los catetos debe ser positivo";
+        } else if (cb < 0) {
+            outputText = "El valor de los catetos debe ser positivo";
+        } else if (isNaN(ca)) {
+            outputText = "El valor de los catetos debe ser numérico";
+        }  else if (isNaN(cb)) {
+            outputText = "El valor de los catetos debe ser numérico";
+        }   else {
+
+            y = Math.sqrt( (Math.pow(ca, 2) + Math.pow(cb, 2)) );
+
+            outputText = "El valor de la hipotenusa es:  <strong>" + y;
+        }
+
+        document.getElementById("output").innerHTML = outputText;
+
+
+    }
